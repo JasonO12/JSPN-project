17470e274a6dd35a1680cf83ed1a585725be05a3
pauldipasquale <pauldipasquale756@gmail.com>
2021-09-22 14:43:41 -0700

    Paul Change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import "packages" from flask
 from flask import Flask, render_template, request
+from algorithm.image import image_data
 
 # create a Flask instance
 app = Flask(__name__)
@@ -95,7 +96,7 @@ def login():
 
 @app.route('/rgb/')
 def rgb():
-    return render_template("rgb.html")
+    return render_template("rgb.html", images=image_data())
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
